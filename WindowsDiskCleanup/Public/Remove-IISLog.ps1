506e45b0d289ed5e4d70097ddceaacecb4143bcf
Alan Płócieniak <alan-null@outlook.com>
2025-03-12 20:04:28 +0100

    Added Remove-IISLog

---
+++ WindowsDiskCleanup/Public/Remove-IISLog.ps1
@@ -0,0 +1,17 @@
+function Remove-IISLog {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [int]$Days,
+
+        [string]$LogFilesFolder = "$env:SystemDrive\inetpub\logs\LogFiles",
+
+        [switch]$DryRun
+    )
+
+    process {
+        if (Test-Path $LogFilesFolder) {
+            Get-ChildItem $LogFilesFolder -Recurse -Filter *.log | Remove-OldFile -Days $Days -DryRun:$DryRun
+        }
+    }
+}
